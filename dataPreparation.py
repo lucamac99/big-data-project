0c23458135c2c1aac813543493f1c80a81d7ad19
lucamac99 <luca.maccacaro14@gmail.com>
2022-06-27 17:24:11 +0200

    upload, clean and plot first data

--- dataPreparation.py
+++ dataPreparation.py
@@ -8,8 +8,14 @@ from datetime import date
 # Importing the dataset
 def data_preparation():
 
-    df = pd.read_csv('housingtarget.csv')
-    #dfPrice = pd.read_csv('housingPrice.csv')
-    #dfIndexes = pd.read_csv('housingIndexes.csv')
+    df_rent = pd.read_csv('final.csv')
 
-    return df
+    # How to compose the final dataframe
+    #df1 = pd.read_csv('housingtarget.csv')
+    #df2 = pd.read_csv('housingtarget (2).csv')
+    #df3 = pd.read_csv('housingtarget (3).csv')
+    #frames = [df1, df2, df3]
+    #result = pd.concat(frames)
+    #result.to_csv('final.csv', sep='\t')
+
+    return df_rent
