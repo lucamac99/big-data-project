acd39dba750af857564e817f194d9d41da27851a
simoneb1x <moneb1x@gmail.com>
2022-06-28 12:49:26 +0200

    redis poc implemented. needs to be adapted to our needs

---
+++ redis/schema.py
@@ -0,0 +1,11 @@
+from redis_om import HashModel
+from config import redis_db
+
+class Task(HashModel):
+    name: str
+    description: str
+    
+    class Meta: 
+        database: redis_db
+
+        
